4611a0bcb8a28fe0640d88f3e7c99a2a17720dd2
ehammo <ehammo@cin.ufpe.br>
2017-07-15 13:00:30 -0300

    Not using apache for equals anymore

--- src/Backup.java
+++ src/Backup.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.stream.Stream;
-import org.apache.commons.io.FileUtils;
 
 /**
  * Created by eduardo on 14/07/2017.
